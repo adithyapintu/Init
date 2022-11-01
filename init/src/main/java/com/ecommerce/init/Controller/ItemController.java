43e685a4a5e78d0ed0fd5b5b48a58c30c1ebd5a9
Adithya <bairiadithya8@gmail.com>
2022-11-02 00:25:13 +0530

    Commit

--- init/src/main/java/com/ecommerce/init/Controller/ItemController.java
+++ init/src/main/java/com/ecommerce/init/Controller/ItemController.java
@@ -1,13 +1,21 @@
 package com.ecommerce.init.Controller;
 
+import com.ecommerce.init.DTO.ItemDTO;
+import com.ecommerce.init.Service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 @RestController
 public class ItemController {
-    
+
+    @Autowired
+    ItemService service;
     @GetMapping(value="/getItem")
     public String getItem(@RequestParam int id){
         return "";
@@ -17,6 +25,11 @@ public class ItemController {
     public String testing(){
         return "Working Fine";
     }
-    
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(value = "/getAllItems")
+    public List<ItemDTO> getAllItems(){
+        return service.getAllItems();
+    }
     
 }
